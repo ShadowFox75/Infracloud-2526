9efd0ca9bb7e1b407b9633986dbede134e786ca7
NetAcad DEVASC <changeme@example.com>
2026-01-19 10:13:50 +0000

    login app experimental version

---
+++ Experimental_stuff/Login_app_exp/login_app_exp.sh
@@ -0,0 +1,17 @@
+# test script for flask_app_login.py -- use in Virtualbox VM, not in Docker
+# programm flask_app_login needs to be running
+echo "Deleting all test records"
+curl -k -X DELETE "http://127.0.0.1:5580/delete/all"
+INSECURE_USER="ICHIGO"
+INSECURE_PW="Kurosaki123"
+SECURE_USER="Luffy"
+SECURE_PW="Monkey123"
+echo "Creating new user v1 insecure"
+curl -k -X POST -F "username="$INSECURE_USER -F "password="$INSECURE_PW "http://127.0.0.1:5580/signup/v1"
+echo "Testing login v1"
+curl -k -X POST -F "username="$INSECURE_USER -F "password="$INSECURE_PW "http://127.0.0.1:5580/login/v1"
+#### Replace fixed username with a variable SECURE_USER and a variable passord SECURE_PW
+echo "Creating new user v2 secure"
+curl -k -X POST -F "username="$SECURE_USER -F "password="$SECURE_PW "http://127.0.0.1:5580/signup/v2"
+echo "Testing login v2"
+curl -k -X POST -F "username="$SECURE_USER -F "password="$SECURE_PW "http://127.0.0.1:5580/login/v2"
---
+++ Experimental_stuff/README.md
@@ -0,0 +1,7 @@
+these codes were used for practice
+
+
+
+
+
+Shadowfox 23:06 18/01/2026
